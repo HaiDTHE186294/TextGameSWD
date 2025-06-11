feed444eee915689d7848dc09deaea1591d94a03
HaiDTHE186294 <haidthe186294@fpt.edu.vn>
2025-06-11 19:27:45 +0700

    test SonarCloud 3

--- src/main/java/org/example/HelloFX.java
+++ src/main/java/org/example/HelloFX.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class HelloFX extends Application {
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Hello, JavaFX 22!");
+        Label label = new Label("Hello, JavaFX 221!");
         Scene scene = new Scene(label, 320, 200);
         stage.setTitle("JavaFX Test");
         stage.setScene(scene);
